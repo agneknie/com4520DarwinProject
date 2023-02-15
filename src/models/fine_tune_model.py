3346e57ad5efadbbf0e2d8a4e296baa19f84ffc9
Adam Allsebrook <adsorigin@gmail.com>
2023-02-15 12:59:23 +0000

    Fine tune model can save every epoch

--- src/models/fine_tune_model.py
+++ src/models/fine_tune_model.py
@@ -26,6 +26,11 @@ Parameters:
         Number of epochs to train for. Default is 4.
     warmup : float
         Amount of the training data to use for warmup.
+    checkpoint_path : str
+        Path to save model checkpoints at. Default is None.
+    checkpoint_save_steps : int
+        How often to save a checkpoint. Default is None. If None and `checkpoint_path` is not None 
+        then a checkpoint is saved after every epoch.
     transform : (list[str], list[str]) -> list[str]
         Takes a list of sentences and corresponding MWEs, should return a transformed list of sentences 
 
@@ -35,7 +40,8 @@ Returns:
 """
 def fine_tune_model(model_path, output_path, train_file,
         tokenize_idioms=False, languages=['EN', 'PT'],
-        batch_size=4, num_epochs=4, warmup=0.1, transform=None):
+        batch_size=4, num_epochs=4, warmup=0.1, checkpoint_path=None, checkpoint_save_steps=None,
+        transform=None):
     
     model = SentenceTransformer(model_path)
     header, data = load_dataset(train_file, tokenize_idioms=tokenize_idioms, transform=transform, languages=languages)
@@ -60,6 +66,8 @@ def fine_tune_model(model_path, output_path, train_file,
     print('Total samples: ', len(positives_dataset) + len(triplets_dataset))
 
     train_objectives = [(positives_dataloader, postitives_loss), (triplets_dataloader, triplets_loss)]
+    if checkpoint_path is not None and checkpoint_save_steps is None:
+        checkpoint_save_steps = min([len(obj[0]) for obj in train_objectives])
 
     warmup_steps = math.ceil(len(positives_dataloader) * num_epochs * warmup) # By default 10% of train data for warm-up
     print('Num warmup steps: ', warmup_steps)
@@ -70,7 +78,9 @@ def fine_tune_model(model_path, output_path, train_file,
         epochs=num_epochs,
         evaluation_steps=0,
         warmup_steps=warmup_steps,
-        output_path=output_path
+        output_path=output_path,
+        checkpoint_path=checkpoint_path,
+        checkpoint_save_steps=checkpoint_save_steps
         )
 
     return model
@@ -97,6 +107,11 @@ Parameters:
         Number of epochs to train for. Default is 4.
     warmup : float
         Amount of the training data to use for warmup.
+    checkpoint_path : str
+        Path to save model checkpoints at. Default is None.
+    checkpoint_save_steps : int
+        How often to save a checkpoint. Default is None. If None and `checkpoint_path` is not None 
+        then a checkpoint is saved after every epoch.
     transform : (list[str], list[str]) -> list[str]
         Takes a list of sentences and corresponding MWEs, should return a transformed list of sentences 
 
@@ -106,7 +121,8 @@ Returns:
 """
 def fine_tune_model_baseline(model_path, output_path, train_file,
         tokenize_idioms=False, languages=['EN', 'PT'],
-        batch_size=4, num_epochs=4, warmup=0.1, transform=None):
+        batch_size=4, num_epochs=4, warmup=0.1, checkpoint_path=None, checkpoint_save_steps=None,
+        transform=None):
     
     model = SentenceTransformer(model_path)
     header, data = load_dataset(train_file, tokenize_idioms=tokenize_idioms, transform=transform, languages=languages)
@@ -120,6 +136,8 @@ def fine_tune_model_baseline(model_path, output_path, train_file,
     print('Num training samples: ', len(dataset))
 
     train_objectives = [(dataloader, loss)]
+    if checkpoint_path is not None and checkpoint_save_steps is None:
+        checkpoint_save_steps = min([len(obj[0]) for obj in train_objectives])
 
     warmup_steps = math.ceil(len(dataloader) * num_epochs * warmup) # By default 10% of train data for warm-up
     print('Num warmup steps: ', warmup_steps)
@@ -130,7 +148,9 @@ def fine_tune_model_baseline(model_path, output_path, train_file,
         epochs=num_epochs,
         evaluation_steps=0,
         warmup_steps=warmup_steps,
-        output_path=output_path
+        output_path=output_path,
+        checkpoint_path=checkpoint_path,
+        checkpoint_save_steps=checkpoint_save_steps
         )
 
     return model
