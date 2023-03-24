57b054970f82959663e4b160897440240c80fbab
Adam Allsebrook <aca19aa@bessemer-login1.shef.ac.uk>
2023-03-24 15:06:58 +0000

    Add fine tune shell script

---
+++ hpc_fine_tune.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+# 16GB of RAM
+#SBATCH --mem=16G
+# email 
+#SBATCH --mail-user=aallsebrook1@sheffield.ac.uk
+#SBATCH --mail-type=BEGIN,END,FAIL
+# set job name
+#SBATCH --job-name=fine_tune_bronze
+#SBATCH --output=R-%x.%j.out
+# get gpus
+#SBATCH --partition=gpu
+#SBATCH --qos=gpu
+#SBATCH --nodes=1
+#SBATCH --gpus-per-node=1
+
+module load Anaconda3/5.3.0
+module load cuDNN/7.6.4.38-gcccuda-2019b
+
+conda create -n pytorch python=3.6
+
+source activate pytorch
+
+pip install torch torchvision
+
+dir=$(pwd)
+
+git clone https://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@github.com/agneknie/com4520DarwinProject.git
+cd com4520DarwinProject
+git checkout framework-hpc
+pip install -r requirements.txt
+conda-develop $dir/com4520DarwinProject/src
+
+python fine_tune_model.py --en --num-epochs=1 --batch-size=32 --seed=4 --tokenize-idioms --base-model="$dir/base_model_tokenized" --train-file="$dir/bronze.csv" --output-path="$dir/model_bronze"
+
