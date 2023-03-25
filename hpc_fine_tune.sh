beae5070dbc0541a5eee59f74f7d88091c8775a7
Adam Allsebrook <adsorigin@gmail.com>
2023-03-25 10:40:04 +0000

    Update hpc fine tune script

--- hpc_fine_tune.sh
+++ hpc_fine_tune.sh
@@ -5,9 +5,10 @@
 #SBATCH --mail-user=aallsebrook1@sheffield.ac.uk
 #SBATCH --mail-type=BEGIN,END,FAIL
 # set job name
-#SBATCH --job-name=fine_tune_bronze
+#SBATCH --job-name=fine_tune_silver_para
+# set output path to [job_name].[job_id].out
 #SBATCH --output=R-%x.%j.out
-# get gpus
+# get gpu
 #SBATCH --partition=gpu
 #SBATCH --qos=gpu
 #SBATCH --nodes=1
@@ -24,11 +25,11 @@ pip install torch torchvision
 
 dir=$(pwd)
 
-git clone https://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@github.com/agneknie/com4520DarwinProject.git
+git clone https://oauth2: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@github.com/agneknie/com4520DarwinProject.git
 cd com4520DarwinProject
 git checkout framework-hpc
 pip install -r requirements.txt
 conda-develop $dir/com4520DarwinProject/src
 
-python fine_tune_model.py --en --num-epochs=1 --batch-size=32 --seed=4 --tokenize-idioms --base-model="$dir/base_model_tokenized" --train-file="$dir/bronze.csv" --dev-path="$dir/com4520DarwinProject/data/datasets/SemEval_2022_Task2_SubTaskB/EvaluationData" --output-path="$dir/model_bronze"
+python fine_tune_model.py --en --include-para-context --num-epochs=4 --batch-size=32 --seed=4 --tokenize-idioms --base-model="$dir/base_model_tokenized" --train-file="$dir/silver_1.csv" --dev-path="$dir/com4520DarwinProject/data/datasets/SemEval_2022_Task2_SubTaskB/EvaluationData" --output-path="$dir/model_silver_para"
 
